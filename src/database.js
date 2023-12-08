0a3c8903f6405ee72e06f6aec820b81c51a2a25d
SpyderCode <Spinneowo@gmail.com>
2023-12-07 18:40:52 -0600

    Conexion a MongoDb Atlas

--- src/database.js
+++ src/database.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
+require ('dotenv').config();
 
-mongoose.connect("mongodb://127.0.0.1/notasdb")
+//const url = "mongodb://127.0.0.1/notasdb"
+const url = process.env.MONGODB_URL;
+
+mongoose.connect(url)
     .then( db => console.log("Base de datos conectada"))
     .catch(err => console.error(err));
